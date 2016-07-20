58634de258bff2c0fc3ab257070578df0ce3776c
daniloluca <danilolucasmd@gmail.com>
2016-07-20 16:00:45 -0300

    Implemented function devideset.

--- src/ml.lua
+++ src/ml.lua
@@ -7,9 +7,35 @@ end
 function ml.train(features, label)
 	return function(input)
 		local output = input
-		
+
 		return output
 	end
 end
+
+function ml.devideset(rows, column, value)
+	local split_function;
+	if type(value) == "number" then
+		split_function = function(row)
+			return row[column] >= value
+		end
+	elseif type(value) == "string" then
+		split_function = function(row)
+			return row[column] == value
+		end
+	end
+
+	local set1 = {}
+	local set2 = {}
+	for i=1, #rows, 1 do
+		if split_function(rows[i]) then
+			set1[#set1+1] = rows[i]
+		else
+			set2[#set2+1] = rows[i]
+		end
+	end
+
+	return set1, set2
+end
+
 
 return ml
--- src/script.lua
+++ src/script.lua
@@ -1,5 +1,41 @@
 local ml = require "ml"
 
-local func = ml.train()
-print(func)
-print(func('test'))
+local data = {
+	{"slashdot","USA","yes",18,"None"},
+    {"google","France","yes",23,"Premium"},
+    {"digg","USA","yes",24,"Basic"},
+    {"kiwitobes","France","yes",23,"Basic"},
+    {"google","UK","no",21,"Premium"},
+    {"(direct)","New Zealand","no",12,"None"},
+    {"(direct)","UK","no",21,"Basic"},
+    {"google","USA","no",24,"Premium"},
+    {"slashdot","France","yes",19,"None"},
+    {"digg","USA","no",18,"None"},
+    {"google","UK","no",18,"None"},
+    {"kiwitobes","UK","no",19,"None"},
+    {"digg","New Zealand","yes",12,"Basic"},
+    {"slashdot","UK","no",21,"None"},
+    {"google","UK","yes",18,"Basic"},
+    {"kiwitobes","France","yes",19,"Basic"},
+}
+
+function print_t(t)
+	local s = {"return {\n"}
+	for i=1,#t do
+	  s[#s+1] = "\t{"
+	  for j=1,#t[i] do
+	    s[#s+1] = t[i][j]
+	    s[#s+1] = ","
+	  end
+	  s[#s+1] = "},\n"
+	end
+	s[#s+1] = "}"
+	s = table.concat(s)
+
+	print(s)
+end
+
+local a, b = ml.devideset(data, 4, 20)
+
+print_t(a)
+print_t(b)
