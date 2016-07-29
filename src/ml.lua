9890dad6b45cad90b203ca651d3b8c6bf91c656d
daniloluca <danilolucasmd@gmail.com>
2016-07-29 15:19:38 -0300

    Implemented functions: simple, multiple and gain.

--- src/ml.lua
+++ src/ml.lua
@@ -1,155 +1,86 @@
 local ml = {}
 
-function ml.test()
-	print('ok')
+function log(x)
+	if x == 0 then
+		return 0
+	end
+	return math.log(x)/math.log(2)
 end
 
-function ml.train(features, label)
-	return function(input)
-		local output = input
-
-		return output
+function entropy(set)
+	local entr = 0
+	for key, value in pairs(set) do
+		entr = entr + ((-value)*log(value))
 	end
+	return entr
 end
 
--- Divides a set on a specific column. Can handle numeric or nominal values
-function ml.divideset(rows, column, value)
-	-- Make a function that tells us if a row is in the first group (true) or the second group (false)
-	local split_function;
-	-- check if the value is a number i.e int or float
-	if type(value) == "number" then
-		split_function = function(row)
-			return row[column] >= value
+function single(set)
+	local size = 0
+	for key, value in pairs(set) do
+		size = size + value
+	end
+
+	local _set = {}
+	for key, value in pairs(set) do
+		_set[#_set+1] = value/size
+	end
+	return entropy(_set)
+end
+
+function multiple(sets)
+	local _sets = {}
+	local size = 0
+	for ki, vi in pairs(sets) do
+		_sets[#_sets+1] = {
+			entr = single(vi),
+			size = 0
+		}
+		for kj, vj in pairs(vi) do
+			_sets[#_sets].size = _sets[#_sets].size + vj
 		end
-	elseif type(value) == "string" then
-		split_function = function(row)
-			return row[column] == value
+		size = size + _sets[#_sets].size
+	end
+
+	local entr = 0
+	for k, v in pairs(_sets) do
+		entr = entr + ((v.size/size)*v.entr)
+	end
+
+	return entr
+end
+
+function ml.divide(node, branch, cond)
+	local split
+	if type(cond) == "number" then
+		split = function(set)
+			return set[branch] >= cond
+		end
+	elseif type(cond) == "string" then
+		split = function(set)
+			return set[branch] == cond
 		end
 	end
 
-	-- Divide the rows into two sets and return them
-	local set1 = {}
-	local set2 = {}
-	for k, v in pairs(rows) do
-		if split_function(v) then
-			set1[#set1+1] = v
+	local sett = {}
+	local setf = {}
+	for key, value in pairs(node) do
+		if split(value) then
+			sett[#sett+1] = value
 		else
-			set2[#set2+1] = v
+			setf[#setf+1] = value
 		end
 	end
 
-	return set1, set2
+	return sett, setf
 end
 
--- Create counts of possible results (the last column of each row is the result)
-function ml.uniquecounts(rows)
-	local results = {}
-	for k, v in pairs(rows) do
-		-- The result is the last column
-		r=v[#v]
-		if results[r] == nil then
-			results[r] = 0
-		end
-		results[r] = results[r] + 1
-	end
-	return results
+function ml.gain(s, m)
+	return single(s) - multiple(m)
 end
 
--- Entropy is the sum of p(x)log(p(x)) across all
--- the different possible results
-function ml.entropy(rows)
-	local log2 = function(x)
-		return math.log(x)/math.log(2)
-	end
-	local results = ml.uniquecounts(rows)
-	-- Now calculate the entropy
-	local ent = 0.0
-	for k, v in pairs(results) do
-		local p = tonumber(results[k]/#rows)
-		ent = ent-p*log2(p)
-	end
-	return ent
-end
+function ml.tree(features, labels)
 
-ml.decisionnode = {}
-ml.decisionnode.__index = ml.decisionnode
-function ml.decisionnode.create(col, value, results, tb, fb)
-	local self = {}
-	setmetatable(self, ml.decisionnode)
-	self.col = col
-	self.value = value
-	self.results = results
-	self.tb = tab
-	self.fb = fb
-
-	if col ~= nil then
-		self.col = -1
-	end
-	if value ~= nil then
-		self.value = {}
-	end
-	if results ~= nil then
-		self.results = {}
-	end
-	if tb ~= nil then
-		self.tb = {}
-	end
-	if fb ~= nil then
-		self.fb = {}
-	end
-	return self
-end
-
-function ml.buildtree(rows, scoref) -- rows is the set, either whole dataset or part of it in the recursive call,
-	-- scoref is the method to measure heterogeneity. By default it's entropy.
-	if scoref == nil then
-		scoref = ml.entropy
-	end
-	-- len(rows) is the number of units in a set
-	if #rows == 0 then
-		return ml.decisionnode()
-	end
-	local current_score = scoref(rows)
-
-	-- Set up some variables to track the best criteria
-	local best_gain = 0.0
-	local best_criteria = {}
-	local best_sets = {}
-
-	-- count the # of attributes/columns
-	-- It's -1 because the last one is the target attribute and it does not count.
-	column_count = (#rows[1])-1
-	for col=1, column_count do
-		-- Generate the list of all possible different values in the considered column
-		column_values = {}
-		for k, row in pairs(rows) do
-			column_values[row[col]] = 1
-			-- Now try dividing the rows up for each value in this column
-			for value in pairs(column_values) do -- the 'values' here are the keys of the dictionnary
-				local set1, set2 = ml.divideset(rows, col, value) -- define set1 and set2 as the 2 children set of a division
-
-				-- Information gain
-				local p = tonumber(#set1/#rows) -- p is the size of a child set relative to its parent
-				local gain = current_score-p*scoref(set1)-(1-p)*scoref(set2) -- cf. formula information gain
-				if gain > best_gain and #set1 > 0 and #set2 > 0 then -- set must not be empty
-					best_gain = gain
-					best_criteria[1] = col
-					best_criteria[2] = value
-					best_sets[1] = set1
-					best_sets[2] = set2
-				end
-			end
-		end
-	end
-
-	-- Create the sub branches
-	if best_gain > 0 then
-		local trueBranch = ml.buildtree(best_sets[1])
-		local falseBranch = ml.buildtree(best_sets[2])
-		return ml.decisionnode.create(best_criteria[1], best_criteria[2], nil, trueBranch, falseBranch)
-	else
-		return ml.decisionnode.create(nil, nil, ml.uniquecounts(rows), nil, nil)
-	end
 end
 
 return ml
--- src/script.lua
+++ src/script.lua
@@ -1,23 +1,23 @@
 local ml = require "ml"
 
-local data = {
-	{"slashdot","USA","yes",18,"None"},
-    {"google","France","yes",23,"Premium"},
-    {"digg","USA","yes",24,"Basic"},
-    {"kiwitobes","France","yes",23,"Basic"},
-    {"google","UK","no",21,"Premium"},
-    {"(direct)","New Zealand","no",12,"None"},
-    {"(direct)","UK","no",21,"Basic"},
-    {"google","USA","no",24,"Premium"},
-    {"slashdot","France","yes",19,"None"},
-    {"digg","USA","no",18,"None"},
-    {"google","UK","no",18,"None"},
-    {"kiwitobes","UK","no",19,"None"},
-    {"digg","New Zealand","yes",12,"Basic"},
-    {"slashdot","UK","no",21,"None"},
-    {"google","UK","yes",18,"Basic"},
-    {"kiwitobes","France","yes",19,"Basic"},
+local features = {
+	{"Sunny", "Hot", "High", "False"},
+	{"Sunny", "Hot", "High", "True"},
+	{"Overcast", "Hot", "High", "False"},
+	{"Rainy", "Mild", "High", "False"},
+	{"Rainy", "Cool", "Normal", "False"},
+	{"Rainy", "Cool", "Normal", "True"},
+	{"Overcast", "Cool", "Normal", "True"},
+	{"Sunny", "Mild", "High", "False"},
+	{"Sunny", "Cool", "Normal", "False"},
+	{"Rainy", "Mild", "Normal", "False"},
+	{"Sunny", "Mild", "Normal", "True"},
+	{"Overcast", "Mild", "High", "True"},
+	{"Overcast", "Hot", "Normal", "False"},
+	{"Rainy", "Mild", "High", "True"}
 }
+
+local labels = {"No", "No", "Yes", "Yes", "Yes", "No", "Yes", "No", "Yes", "Yes", "Yes", "Yes", "Yes", "No"}
 
 function print_t(t)
 	local s = {"return {\n"}
@@ -43,53 +43,14 @@ function print_k(t)
 	print("}")
 end
 
-function printtree(tree)
-	if tree.results ~= nil then
-		print(tostring(tree.results))
-	else
-		print(tostring(tree.col)..":"..tostring(tree.value).."? ")
-		print(" T->  ")
-		printtree(tree.tb, "   ")
-		print(indent.."F->  ")
-		printtree(tree.fb, "   ")
-	end
-end
-
--- local a, b = ml.divideset(data, 4, 20)
-
+-- output
+-- local a, b = ml.divide(features, 1, "Sunny")
+--
 -- print_t(a)
 -- print_t(b)
 
--- print_k(ml.uniquecounts(data));
+local s = {9, 5}
+local m = {{2, 3}, {4, 0}, {3, 2}}
+local a = ml.gain(s, m)
 
--- local a, b = ml.divideset(data,4,20)
--- print_t(a)
--- print_k(ml.uniquecounts(a))
--- print("")
--- print_t(b)
--- print_k(ml.uniquecounts(b))
-
--- local set1, set2=ml.divideset(data,4,20)
--- print(ml.entropy(set1), ml.entropy(set2))
-
--- print(ml.entropy(data))
-
-
-local tree=ml.buildtree(data)
-
-printtree(tree);
--- print(tree.col)
--- print(tree.value)
--- print(tree.results)
--- print("")
--- print(tree.tb.col)
--- print(tree.tb.value)
--- print(tree.tb.results)
--- print("")
--- print(tree.tb.tb.col)
--- print(tree.tb.tb.value)
--- print(tree.tb.tb.results)
--- print("")
--- print(tree.tb.fb.col)
--- print(tree.tb.fb.value)
--- print(tree.tb.fb.results)
+print(a)
